7a63ac5cd80fc6c61b26905bf40c253cb87d00e6
pongpatapee <pongpatapee1021@gmail.com>
2025-01-11 15:52:13 +0700

    feat: fixed card height, limit title length

--- src/components/MangaItem.jsx
+++ src/components/MangaItem.jsx
@@ -1,11 +1,17 @@
 const MangaItem = ({ mangaData }) => {
+  const maxTitleLength = 20;
+
   return (
     <div className="mangaItem">
       <img
         src={mangaData.images.jpg.image_url}
         alt={`${mangaData.title} image`}
       />
-      <h1>{mangaData.title}</h1>
+      <h1>
+        {mangaData.title.length <= maxTitleLength
+          ? mangaData.title
+          : `${mangaData.title.substring(0, maxTitleLength)}...`}
+      </h1>
 
       <div>
         <p>Rank: {mangaData.rank}</p>
