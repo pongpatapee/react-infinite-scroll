dc11a8d3f4db60b27abf069f940801931bd1c195
pongpatapee <pongpatapee1021@gmail.com>
2025-01-11 15:46:26 +0700

    feat: infinite scrolling
    Scrolling to the bottom fetches more data

--- src/components/MangaItem.jsx
+++ src/components/MangaItem.jsx
@@ -1,11 +1,11 @@
 const MangaItem = ({ mangaData }) => {
   return (
     <div className="mangaItem">
-      <h1>{mangaData.title}</h1>
       <img
         src={mangaData.images.jpg.image_url}
         alt={`${mangaData.title} image`}
       />
+      <h1>{mangaData.title}</h1>
 
       <div>
         <p>Rank: {mangaData.rank}</p>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Strict mode render components twice in dev mode, leading to double useEffects
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
+
+createRoot(document.getElementById("root")).render(<App />);
